031a770ac200ba2f7061135b8b6865941749be5a
20101642 <20101642@P-2-60-176730>
2023-05-25 11:09:05 +0800

    Add project files.

---
+++ IOTProjectAT3/DBSystem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTProjectAT3
+{
+    /// <summary>
+    /// Handle all the SQL queries and directly interface with the DB
+    /// </summary>
+    public class DBSystem
+    {
+        public const string DbServer = "localhost";
+        public const string DbUser = "root";
+        public const string DbName = "lmm_ictprg402";
+        public const int DbPort = 3306;
+        public const string DbPassword = "password";
+        public static readonly string DbConnectionString = $"server={DbServer}; user={DbUser}; database={DbName}; port={DbPort}; password={DbPassword}";
+
+        //TODO: figure out if I need to do anything with a constructor
+        public DBSystem() 
+        {
+
+        }
+
+        //Select from all tables 
+        public void SelectTable()
+        {
+
+        }
+
+        //Populate list box automatically
+        public void FillList()
+        {
+        }
+
+        //Perform like query on table 
+        public void SearchList()
+        {
+
+        }
+        
+        //Perform update query on record
+        public void EditRecord()
+        {
+
+        }
+        
+        //Perform Join query on table
+        public void JoinTable()
+        {
+
+        }
+
+        //Insert new record into table
+        public void InsertRecord()
+        {
+
+        }
+
+        //Delete currently selected record
+        public void DeleteRecord() 
+        {
+
+        }
+
+    }
+}
---
+++ IOTProjectAT3/EmployeeTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTProjectAT3
+{
+    /// <summary>
+    /// Define and return properties of the Employees table
+    /// </summary>
+    internal class EmployeeTable : DBSystem
+    {
+        
+
+
+    }
+}
